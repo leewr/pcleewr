0b7ff24c265a1e5d101809936d92eac05deba7df
leewr <121657771@qq.com>
2019-01-17 22:33:53 +0800

    change

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import roorReducer from './reducers'
+import rootReducer from './reducers'
 
 let middleware = [thunk]
 
---
+++ src/store/reducers/user.js
@@ -0,0 +1,8 @@
+export function user(state = initialState, action) {
+    switch(action.type) {
+        case '':
+            return ''
+        default:
+            return state
+    }
+}
